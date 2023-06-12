8a00c71e797a7ea74c96bce59c5c2651a3c2c0fb
anmspro <sakib.anms@gmail.com>
2023-06-12 11:04:29 +0600

    home page design update

---
+++ src/homepage/index.js
@@ -0,0 +1,16 @@
+function startGame() {
+    window.location.href = "./../game/home.html";
+  }
+  
+  function viewLeaderboard() {
+    window.location.href = "./../leaderboard.html";
+  }
+  
+  function getRewards() {
+    window.location.href = "rewards.html";
+  }
+  
+  function logOut() {
+    window.location.href = "logout.html";
+  }
+  
