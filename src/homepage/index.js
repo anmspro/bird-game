26bad20f2b55dbd9004403885d784ad648843c25
anmspro <sakib.anms@gmail.com>
2023-06-12 11:18:33 +0600

    project restructured

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -3,14 +3,14 @@ function startGame() {
   }
   
   function viewLeaderboard() {
-    window.location.href = "./../leaderboard.html";
+    window.location.href = "./../leaderboard/leaderboard.html";
   }
   
   function getRewards() {
-    window.location.href = "rewards.html";
+    // window.location.href = "rewards.html";
   }
   
   function logOut() {
-    window.location.href = "logout.html";
+    // window.location.href = "logout.html";
   }
   
